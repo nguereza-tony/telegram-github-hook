b15e78a543c665cead3dfe8293d1eecdcc2a4ef2
NGUEREZA Tony <nguerezatony@gmail.com>
2025-10-04 11:35:29 +0100

    Add demo test

---
+++ tests/AppTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Quantum\Test;
+
+use Platine\Dev\PlatineTestCase;
+
+/**
+ * App class tests
+ *
+ * @group core
+ */
+class AppTest extends PlatineTestCase
+{
+    public function testAll(): void
+    {
+        $this->assertTrue(true);
+    }
+}
